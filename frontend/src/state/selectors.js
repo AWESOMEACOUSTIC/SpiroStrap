184caa89611097de005d81c3360fa6cecfa6ccf4
Aditya <adityapanigrahy98@gmail.com>
2026-01-24 04:34:54 +0530

    added current windows, scores or labels

--- frontend/src/state/selectors.js
+++ frontend/src/state/selectors.js
@@ -1,5 +1,10 @@
 export const selectStreaming = (s) => s.streaming;
 export const selectSessionId = (s) => s.sessionId;
 export const selectStartedAt = (s) => s.startedAt;
+
 export const selectSamples = (s) => s.samples;
 export const selectLastSample = (s) => s.lastSample;
+
+export const selectWindows = (s) => s.windows;
+export const selectCurrentLabel = (s) => s.currentLabel;
+export const selectCurrentScore = (s) => s.currentScore;
